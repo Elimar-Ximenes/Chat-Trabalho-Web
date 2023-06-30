e1f22d5dc7685c221dd9635aafb751757ce2f4f7
Elimar-Ximenes <66808649+Elimar-Ximenes@users.noreply.github.com>
2023-06-30 17:46:19 -0300

    Add files via upload

--- chat-interface-angular/src/app/home/home.component.ts
+++ chat-interface-angular/src/app/home/home.component.ts
@@ -11,16 +11,46 @@ export class HomeComponent implements OnInit {
   private API_URL = 'http://localhost:8080/api/usuario';
   users: any[] = [];
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+  ) {}
+
+  userEmail = localStorage.getItem('userEmail');
+  userFoto = localStorage.getItem('userFoto');
+  userNome = localStorage.getItem('userNome');
+
 
   ngOnInit(): void {
-    this.http.get<any[]>(this.API_URL).subscribe(users => {
-      this.users = users;
-    });
+    if (this.users.length === 0) {
+      this.loadUsers();
+    }
   }
-
+  
+  
   startChat(user: any): void {
     const email = user.email;
     this.router.navigate(['/chat', email]);
   }
+  
+  private loadUsers(): void {
+    this.userEmail = localStorage.getItem('userEmail');
+    
+    this.http.get<any[]>(this.API_URL + '/except?email=' + this.userEmail).subscribe(users => {
+      this.users = users;
+    });
+  }
+
+  searchQuery: string = ''; // Propriedade para armazenar o termo de pesquisa
+  searchedUsers: any[] = []; // Propriedade para armazenar os usuários encontrados na pesquisa
+  
+  searchUsers(): void {
+    // Implemente a lógica para realizar a pesquisa de usuários com base no termo searchQuery
+    // Por exemplo, você pode chamar um serviço HTTP para fazer a consulta no backend
+  
+    // Supondo que você obtenha os usuários encontrados na variável searchedUsers
+    this.searchedUsers = []; // Limpa os resultados anteriores
+    // Lógica para buscar os usuários com base no termo de pesquisa e armazenar em searchedUsers
+  }
+
 }
--- chat-interface-angular/src/app/login/login.component.ts
+++ chat-interface-angular/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginService } from '../login.service';
 import { StorageService } from '../storage.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,11 @@ export class LoginComponent implements OnInit{
   email: string = ''; // Valor inicial vazio
   password: string = ''; // Valor inicial vazio
 
-  constructor(private loginService: LoginService, private router: Router, private localStorage: StorageService) {}
+  constructor(
+    private loginService: LoginService, 
+    private router: Router, 
+    private localStorage: StorageService, 
+    private http: HttpClient) {}
  
   ngOnInit(): void {
   }
@@ -22,8 +27,23 @@ export class LoginComponent implements OnInit{
     this.loginService.login(this.email, this.password).subscribe((user) => {
       this.router.navigate(['/home']);
       this.localStorage.set('authorization', btoa(this.email + ':' + this.password));
+      
+      const apiUrl = 'http://localhost:8080/api/usuario/verificar';
+      this.http.get<any>(`${apiUrl}?email=${this.email}`).subscribe((response) => {
+      const usuario = response; // Armazena o valor da resposta na variável 'usuario'
+      
+      localStorage.setItem('userEmail', this.email);
+      localStorage.setItem('userNome', usuario.nome);
+      localStorage.setItem('userFoto', usuario.foto);
+   
+      }, (error) => {
+        console.log(error);
+        alert('Usuario ou senha invalidos. Tente novamente');
+      });
+     
       console.log(user);
     }, (error) => {
+      console.log(error);
       alert('Usuario ou senha invalidos. Tente novamente');
     });
   }
